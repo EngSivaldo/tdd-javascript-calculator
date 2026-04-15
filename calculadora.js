1fd8907108546795b7fa092a4b19d7024a84cb4a
Sivaldo Almeida <sivaldovieiradealmeida@gmail.com>
2026-04-15 20:10:45 -0300

    fix: implementando limites de desconto e passando nos testes

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,9 @@
 function calcularDesconto(valor, porcentagem) {
+    // Se a porcentagem for maior que 100 ou menor que 0, ignora o desconto
+    if (porcentagem > 100 || porcentagem < 0) {
+        return valor;
+    }
     return valor - (valor * (porcentagem / 100));
 }
 
-// O erro costuma estar aqui. Verifique se as chaves {} estão presentes.
 module.exports = { calcularDesconto };
